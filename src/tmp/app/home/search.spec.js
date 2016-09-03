5714b6cc6789db0b2af6f0d1ec72f459cbfcac2d
Ajey <ajey.charantimath@gmail.com>
2016-09-04 02:25:06 +0530

    some style fixes

--- src/tmp/app/home/search.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-describe('Search Validation', function () {
-    it('has name', function () {
-        var search = { text: 'ajeey' };
-        expect(search.text).toEqual('ajeey');
-    });
-});
-
-//# sourceMappingURL=search.spec.js.map
